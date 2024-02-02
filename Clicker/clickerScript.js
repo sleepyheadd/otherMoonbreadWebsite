1dc5cb34067788cc3773a31f51398b83b96f9289
sleepyheadd <dmbeck9459@gmail.com>
2024-02-01 19:31:49 -0500

    CLICKER GAME

--- Clicker/clickerScript.js
+++ Clicker/clickerScript.js
@@ -1,6 +1,7 @@
 var total = 0;
 
 var perClick = 1;
+var autoClick = 0;
 
 function ChocClicked() {
     var choccros = document.getElementById("croissant");
@@ -26,6 +27,7 @@ function IncClick() {
         perClickCost = Math.round(1.5 * perClickCost);
         perClick += 1;
         perClickId.textContent = "(" + perClickCost + ") +" + 1 + " Per Click";
+        document.getElementById("perClickStat").textContent = perClick;
         document.getElementById("choctext").textContent = total;
     } else {
         perClickId.textContent = "Not enough croissants!!";
@@ -33,4 +35,45 @@ function IncClick() {
             perClickId.textContent = "(" + perClickCost + ") +" + 1 + " Per Click";
         }, 600);
     }
+}
+
+var autoClickCost = 100;
+var autoActivated = false;
+
+function IncAutoClick() {
+    var autoClickId = document.getElementById("autoClick");
+
+    if (total >= autoClickCost) {
+        total -= autoClickCost;
+        autoClickCost -= 2;
+        autoClickCost = Math.round(1.5 * autoClickCost);
+        autoClick += 1;
+        if (autoClick == 1 && !autoActivated) {
+            autoActivated = true;
+            AutoClicker();
+        }
+        autoClickId.textContent = "(" + autoClickCost + ") +1 Click Per Second";
+        document.getElementById("autoClickStat").textContent = autoClick;
+        document.getElementById("choctext").textContent = total;
+    } else {
+        autoClickId.textContent = "Not enough croissants!!";
+        setTimeout(function () {
+            autoClickId.textContent = "(" + autoClickCost + ") +1 Click Per Second";
+        }, 600);
+    }
+}
+
+var tempTotal = 0;
+
+function AutoClicker() {
+    tempTotal += autoClick * 10
+    console.log(tempTotal);
+    while (tempTotal >= 100) {
+        tempTotal -= 100; total += 1;
+    }
+    document.getElementById("choctext").textContent = total;
+
+    setTimeout(function () {
+        AutoClicker();
+    }, 100);
 }
