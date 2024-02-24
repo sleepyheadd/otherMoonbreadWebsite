c8af0901df77380b1d3e1568d52ae032e204771f
sleepyheadd <dmbeck9459@gmail.com>
2024-02-24 12:36:28 -0500

    upgrades for clicking game

--- Clicker/clickerScript.js
+++ Clicker/clickerScript.js
@@ -1,4 +1,4 @@
-var total = 0;
+var total = 4000;
 
 var perClick = 1;
 var autoClick = 0;
@@ -17,7 +17,8 @@ function ChocClicked() {
 }
 
 var perClickCost = 50;
-var perClickAmount = 0;
+var perClickAmount = 1;
+var perClickIncrement = 1;
 
 function IncClick() {
     var perClickId = document.getElementById("perClick")
@@ -25,17 +26,17 @@ function IncClick() {
     if (total >= perClickCost) {
         total -= perClickCost;
         perClickCost = Math.round(1.2 * perClickCost);
-        perClick += 1;
+        perClick += perClickIncrement;
         perClickAmount++;
 
-        perClickId.textContent = "(" + perClickCost + ") +" + 1 + " Per Click";
+        perClickId.textContent = "(" + perClickCost + ") +" + perClickIncrement + " Per Click";
         document.getElementById("perClickStat").textContent = perClick;
         document.getElementById("choctext").textContent = total;
         document.getElementById("perClickCount").textContent = perClickAmount;
     } else {
         perClickId.textContent = "Not enough croissants!!";
         setTimeout(function () {
-            perClickId.textContent = "(" + perClickCost + ") +" + 1 + " Per Click";
+            perClickId.textContent = "(" + perClickCost + ") +" + perClickIncrement + " Per Click";
         }, 600);
     }
 }
@@ -43,6 +44,7 @@ function IncClick() {
 var autoClickCost = 100;
 var autoClickAmount = 0;
 var autoActivated = false;
+var BakerIncrement = 1;
 
 function IncAutoClick() {
     var autoClickId = document.getElementById("autoClick");
@@ -50,27 +52,28 @@ function IncAutoClick() {
     if (total >= autoClickCost) {
         total -= autoClickCost;
         autoClickCost = Math.round(1.3 * autoClickCost);
-        autoClick += 1;
+        autoClick += BakerIncrement;
         autoClickAmount++;
-        if (autoClick == 1 && !autoActivated) {
+        if (autoClick == BakerIncrement && !autoActivated) {
             autoActivated = true;
             AutoClicker();
         }
 
-        autoClickId.textContent = "(" + autoClickCost + ") +1 Click Per Second";
+        autoClickId.textContent = "(" + autoClickCost + ") +" + BakerIncrement + " Click Per Second";
         document.getElementById("autoClickStat").textContent = autoClick;
         document.getElementById("choctext").textContent = total;
         document.getElementById("IncAutoCount").textContent = autoClickAmount;
     } else {
         autoClickId.textContent = "Not enough croissants!!";
         setTimeout(function () {
-            autoClickId.textContent = "(" + autoClickCost + ") +1 Click Per Second";
+            autoClickId.textContent = "(" + autoClickCost + ") +" + BakerIncrement + " Click Per Second";
         }, 600);
     }
 }
 
 var storeClickCost = 500;
 var storeClickAmount = 0;
+var StoreIncrement = 7;
 
 function StoreAutoClick() {
     var storeClickId = document.getElementById("storeClick");
@@ -78,27 +81,28 @@ function StoreAutoClick() {
     if (total >= storeClickCost) {
         total -= storeClickCost;
         storeClickCost = Math.round(1.3 * storeClickCost);
-        autoClick += 7;
+        autoClick += StoreIncrement;
         storeClickAmount++;
-        if (autoClick == 7 && !autoActivated) {
+        if (autoClick == StoreIncrement && !autoActivated) {
             autoActivated = true;
             AutoClicker();
         }
 
-        storeClickId.textContent = "(" + storeClickCost + ") +7 Click Per Second";
+        storeClickId.textContent = "(" + storeClickCost + ") +" + StoreIncrement + " Click Per Second";
         document.getElementById("autoClickStat").textContent = autoClick;
         document.getElementById("choctext").textContent = total;
         document.getElementById("StoreAutoCount").textContent = storeClickAmount;
     } else {
         storeClickId.textContent = "Not enough croissants!!";
         setTimeout(function () {
-            storeClickId.textContent = "(" + storeClickCost + ") +7 Clicks Per Second";
+            storeClickId.textContent = "(" + storeClickCost + ") +" + StoreIncrement + " Clicks Per Second";
         }, 600);
     }
 }
 
 var factoryClickCost = 3200;
 var factoryAmount = 0;
+var factoryIncrement = 50;
 
 function FactoryAutoClick() {
     var storeClickId = document.getElementById("factoryClick");
@@ -106,21 +110,21 @@ function FactoryAutoClick() {
     if (total >= factoryClickCost) {
         total -= factoryClickCost;
         factoryClickCost = Math.round(1.3 * factoryClickCost);
-        autoClick += 50;
+        autoClick += factoryIncrement;
         factoryAmount++;
-        if (autoClick == 50 && !autoActivated) {
+        if (autoClick == factoryIncrement && !autoActivated) {
             autoActivated = true;
             AutoClicker();
         }
 
-        storeClickId.textContent = "(" + factoryClickCost + ") +50 Clicks Per Second";
+        storeClickId.textContent = "(" + factoryClickCost + ") +" + factoryIncrement + " Clicks Per Second";
         document.getElementById("autoClickStat").textContent = autoClick;
         document.getElementById("choctext").textContent = total;
         document.getElementById("FactoryAutoCount").textContent = factoryAmount;
     } else {
         storeClickId.textContent = "Not enough croissants!!";
         setTimeout(function () {
-            storeClickId.textContent = "(" + factoryClickCost + ") +50 Clicks Per Second";
+            storeClickId.textContent = "(" + factoryClickCost + ") +" + factoryIncrement + " Clicks Per Second";
         }, 600);
     }
 }
@@ -131,7 +135,7 @@ var tempTotal = 0;
 
 function AutoClicker() {
     tempTotal += autoClick * 10
-    console.log(tempTotal);
+    //console.log(tempTotal);
     while (tempTotal >= 100) {
         tempTotal -= 100; total += 1;
     }
@@ -140,4 +144,62 @@ function AutoClicker() {
     setTimeout(function () {
         AutoClicker();
     }, 100);
+}
+
+var clickUpgradeCost = 500;
+var clickUpgradeAmt = 0;
+
+function doubleClickingPower() {
+    var clickUpgradeText = document.getElementById("clickUpgrade");
+    var perClickId = document.getElementById("perClick");
+
+    if (total >= clickUpgradeCost) {
+        total -= clickUpgradeCost;
+        clickUpgradeCost = Math.round(3.2 * clickUpgradeCost);
+
+        perClick *= 2;
+        perClickIncrement *= 2;
+
+        clickUpgradeAmt++;
+
+        clickUpgradeText.textContent = "(" + clickUpgradeCost + ") + x2 Per Click";
+        perClickId.textContent = "(" + perClickCost + ") +" + perClickIncrement + " Per Click";
+        document.getElementById("perClickStat").textContent = perClick;
+        document.getElementById("choctext").textContent = total;
+        document.getElementById("clickUpgradeCount").textContent = clickUpgradeAmt;
+    } else {
+        clickUpgradeText.textContent = "Not enough croissants!!";
+        setTimeout(function () {
+            clickUpgradeText.textContent = "(" + clickUpgradeCost + ") x2 Per Click";
+        }, 600);
+    }
+}
+
+var bakerUpgradeCost = 2000;
+var bakerUpgradeAmt = 0;
+
+function doubleBakingPower() {
+    var BakerUpgradeText = document.getElementById("bakerUpgrade");
+    var BakerId = document.getElementById("autoClick");
+
+    if (total >= bakerUpgradeCost) {
+        total -= bakerUpgradeCost;
+        bakerUpgradeCost = Math.round(2.3 * bakerUpgradeCost);
+
+        autoClick = autoClick + autoClickAmount * BakerIncrement;
+        BakerIncrement *= 2;
+
+        bakerUpgradeAmt++;
+
+        BakerUpgradeText.textContent = "(" + bakerUpgradeCost + ") x2 Helpful Baker Output";
+        BakerId.textContent = "(" + autoClickCost + ") +" + BakerIncrement + " Click Per Second";
+        document.getElementById("autoClickStat").textContent = autoClick;
+        document.getElementById("choctext").textContent = total;
+        document.getElementById("bakerUpgradeCount").textContent = bakerUpgradeAmt;
+    } else {
+        BakerUpgradeText.textContent = "Not enough croissants!!";
+        setTimeout(function () {
+            BakerUpgradeText.textContent = "(" + bakerUpgradeCost + ") x2 Helpful Baker Output";
+        }, 600);
+    }
 }
